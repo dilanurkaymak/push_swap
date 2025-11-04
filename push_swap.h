c851a092c565cb4c98817468fdca36adab2f5217
dilanurkaymak <dilanurnkaymak@gmail.com>
2025-11-04 23:06:21 +0300

    4 nov

---
+++ push_swap.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkaymak <dkaymak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 22:11:22 by dkaymak          #+#    #+#              */
+/*   Updated: 2025/11/04 22:11:22 by dkaymak        ###   ########.fr         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	int				cost_a;
+	int				cost_b;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+	int		size;
+}	t_stack;
+
+#endif
